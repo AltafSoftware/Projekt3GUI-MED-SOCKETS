72276517111fc3afb142ae18e2f5f90e5ffbde4d
Altaf <altafsoftware01@gmail.com>
2024-04-20 20:08:09 +0200

    app.js kommentering

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,12 +1,17 @@
+// Import the CSS file for styling
 import './index.css';
 
-
+// Define a functional component named App
 function App() {
+  // Return JSX representing the component's UI
   return (
+    // Render a div element with a class name of "App"
     <div className="App">
-        SOME TEXT!
+      {/* Render the text "SOME TEXT!" inside the div */}
+      DRUKSPIL
     </div>
   );
 }
 
+// Export the App component as the default export of this module
 export default App;
