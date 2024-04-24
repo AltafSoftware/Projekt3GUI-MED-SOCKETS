be8fa276debed80d6c93c990c07ff7ad6bc74a51
Altaf <altafsoftware01@gmail.com>
2024-04-24 23:19:45 +0200

    lille ændring.

--- reactproject/public/Registrering.js
+++ reactproject/public/Registrering.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // <---------- DETTE NAVN SKAL LAVES OM!
     const startGameButton = document.getElementById("startSpilKnap");
     startGameButton.disabled = true; // Start disabled
 
